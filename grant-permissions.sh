f2f0a9fb52a9604ed05180e0f64ee74ff5e76575
Jack Wang <jack@meldcx.com>
2020-11-27 10:47:17 +1100

    Added permission screen dismiss

--- grant-permissions.sh
+++ grant-permissions.sh
@@ -16,6 +16,28 @@ adb shell pm grant com.meldcx.appupdater android.permission.WRITE_EXTERNAL_STORA
 sleep 3s
 adb shell pm grant com.meldcx.agentm.service.onboarding android.permission.ACCESS_COARSE_LOCATION
 
+adb shell am start -n "com.meldcx.agentm/com.meldcx.agentm.install.InstallUtil"
+sleep 5s
+if [ "$(adb shell dumpsys window displays | grep -c 'mBounds=\[0,0\]\[1080,1920\]')" -ge 1 ]; then
+    echo "Portrait Dismiss"
+    adb shell input tap 196 948
+    sleep 1s
+    adb shell input tap 820 1040
+    sleep 3s
+    adb shell input tap 1000 400
+    adb shell input keyevent KEYCODE_BACK
+else 
+    echo "Landscape Dismiss"
+    adb shell input tap 500 580
+    sleep 2s
+    adb shell input tap 1250 670
+    sleep 3s
+    adb shell input tap 1745 390
+    adb shell input keyevent KEYCODE_BACK
+fi
+
 adb shell am start-foreground-service com.meldcx.appupdater/.pollingservice.PollingService
 adb shell am start-foreground-service com.meldcx.watchdog/.WatchdogService
+sleep 2s
+adb reboot
 popd || exit
